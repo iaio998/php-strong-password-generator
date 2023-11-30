0e2eed5e49530b3ba20f775d050b81f670df0648
iaio998 <depalatisiacopo@gmail.com>
2023-11-30 15:35:52 +0100

    Working random generator function

--- function/helper.php
+++ function/helper.php
@@ -5,10 +5,10 @@ function generatePass()
     $letters = 'qwertyuiopasdfghjklzxcvbnm';
     $upLetters = strtoupper($letters);
     $numbers = '0123456789';
-    if (isset($_GET['pwLenght'])) {
-        $pwLenght = $_GET['pwLengt'];
+    if (isset($_GET['pwLength'])) {
+        $pwLength = $_GET['pwLength'];
         $newpassword = '';
-        while (strlen($newpassword) < $pwLenght) {
+        while (strlen($newpassword) < $pwLength) {
             $list = $symbols . $letters . $upLetters . $numbers;
             $newChar = $list[rand(0, strlen($list) - 1)];
             if (!strpos($newpassword, $newChar)) {
@@ -17,6 +17,6 @@ function generatePass()
         }
         return $newpassword;
     }
-
 }
+;
 ?>
